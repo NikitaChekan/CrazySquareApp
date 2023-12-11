5ed988cc56488ff64874870c5972602f7feff763
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-12-11 14:10:23 +0300

    square with color inversion to match the background

--- CrazySquareApp/ContentView.swift
+++ CrazySquareApp/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var movingSquare = CGSize.zero
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { proxy in
+            let width = proxy.size.width
+            let height = proxy.size.height / 4
+            
+            VStack(spacing: 0) {
+                Rectangle()
+                    .frame(width: width, height: height)
+                    .foregroundColor(.white)
+                Rectangle()
+                    .frame(width: width, height: height)
+                    .foregroundColor(.pink)
+                Rectangle()
+                    .frame(width: width, height: height)
+                    .foregroundColor(.yellow)
+                Rectangle()
+                    .frame(width: width, height: height)
+                    .foregroundColor(.black)
+            }
+            .overlay() {
+                Rectangle()
+                    .blendMode(.exclusion)
+                    .frame(width: 150, height: 150)
+                    .foregroundColor(.white)
+                    .cornerRadius(15)
+                    .offset(movingSquare)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { movingSquare = $0.translation }
+                            .onEnded { _ in
+                                withAnimation(.spring()) {
+                                    movingSquare = .zero
+                                }
+                            }
+                    )
+            }
         }
-        .padding()
+        .ignoresSafeArea()
+        
     }
 }
 
