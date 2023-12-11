1a818317e934315a5ec1cf6f594a9db1c18d8dbb
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-12-11 12:47:56 +0300

    Initial Commit

---
+++ CrazySquareApp/CrazySquareAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrazySquareAppApp.swift
+//  CrazySquareApp
+//
+//  Created by jopootrivatel on 11.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CrazySquareAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
