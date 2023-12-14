e695d6c7db01f48084ddf2d2093847aa7a2293e0
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-12-14 11:49:01 +0300

    bug fix

--- CrazySquareApp/ContentView.swift
+++ CrazySquareApp/ContentView.swift
@@ -12,56 +12,45 @@ struct ContentView: View {
     @State private var movingSquare = CGSize.zero
     
     var body: some View {
-        GeometryReader { proxy in
-            let width = proxy.size.width
-            let height = proxy.size.height / 4
-            
+        ZStack {
             VStack(spacing: 0) {
-                Rectangle()
-                    .frame(width: width, height: height)
-                    .foregroundColor(.white)
-                Rectangle()
-                    .frame(width: width, height: height)
-                    .foregroundColor(.pink)
-                Rectangle()
-                    .frame(width: width, height: height)
-                    .foregroundColor(.yellow)
-                Rectangle()
-                    .frame(width: width, height: height)
-                    .foregroundColor(.black)
+                Color.white
+                Color.pink
+                Color.yellow
+                Color.black
             }
-            .overlay() {
-                ZStack {
-                    Rectangle()
-                        .blendMode(.exclusion)
-                        .foregroundColor(.white)
-                        .cornerRadius(15)
-                    Rectangle()
-                        .blendMode(.color)
-                        .cornerRadius(15)
-                    Rectangle()
-                        .blendMode(.overlay)
-                        .foregroundColor(.white)
-                        .cornerRadius(15)
-                    Rectangle()
-                        .blendMode(.overlay)
-                        .foregroundColor(.black)
-                        .cornerRadius(15)
-                }
-                .frame(width: 150, height: 150)
-                .offset(movingSquare)
-                .gesture(
-                    DragGesture()
-                        .onChanged { movingSquare = $0.translation }
-                        .onEnded { _ in
-                            withAnimation(.spring()) {
-                                movingSquare = .zero
-                            }
-                        }
-                )
-            }
+            .ignoresSafeArea()
         }
-        .ignoresSafeArea()
+        .overlay() {
+            ZStack {
+                Rectangle()
+                    .blendMode(.exclusion)
+                    .foregroundColor(.white)
+                    .cornerRadius(15)
+                Rectangle()
+                    .blendMode(.color)
+                    .cornerRadius(15)
+                Rectangle()
+                    .blendMode(.overlay)
+                    .foregroundColor(.white)
+                    .cornerRadius(15)
+                Rectangle()
+                    .blendMode(.overlay)
+                    .foregroundColor(.black)
+                    .cornerRadius(15)
+            }
+            .frame(width: 150, height: 150)
+            .offset(movingSquare)
+            .gesture(
+                DragGesture()
+                    .onChanged { movingSquare = $0.translation }
+                    .onEnded { _ in
+                        withAnimation(.spring()) {
+                            movingSquare = .zero
+                        }
+                    }
+            )
+        }
     }
 }
 
